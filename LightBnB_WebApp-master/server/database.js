8664254eec6a94cb930b8e0bced2741b3e9cefdc
Lana <kassembayeva@gmail.com>
2021-06-25 01:31:51 +0000

    the search by properties is complete

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -119,19 +119,71 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      if (result.rows) {
-        return result.rows;
-      }
-      return null;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+
+ const getAllProperties = function (options, limit = 10) {
+  
+  const queryParams = [];
+  
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1 `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND LOWER(city) LIKE LOWER($${queryParams.length}) `;
+  }
+
+  if (options.owner_id){
+    queryParams.push(options.owner_id);
+    queryString += `AND owner_id = $${queryParams.length} `
+  }
+
+  if(options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `AND cost_per_night > $${queryParams.length} `;
+  }
+
+  if(options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `AND cost_per_night < $${queryParams.length} `;
+  }
+
+  queryString += `GROUP BY properties.id`
+
+  if(options.minimum_rating) {
+  queryParams.push(options.minimum_rating);
+  queryString += ` HAVING avg(property_reviews.rating) > $${queryParams.length} `;
+  }
+  
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  
+
+  
+  console.log('queryString: ',queryString, 'queryParams:', queryParams);
+
+  
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
+
+// const getAllProperties = (options, limit = 10) => {
+//   return pool
+//     .query(`SELECT * FROM properties LIMIT $1`, [limit])
+//     .then((result) => {
+//       if (result.rows) {
+//         return result.rows;
+//       }
+//       return null;
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// };
 
 // const limitedProperties = {};
 // for (let i = 1; i <= limit; i++) {
